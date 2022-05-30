18da36f27079ddea7cf50fdc10456af917a4e9e4
Eric Bishop <futureflyer.eric@gmail.com>
2022-05-29 20:55:32 -0600

    feat: Day 4
    The neatness of everything kinda started to fall apart towards the end, oops :^)

---
+++ src/solutions/day_5.rs
@@ -0,0 +1,207 @@
+use std::ops::RangeInclusive;
+
+use crate::common::Solution;
+
+pub struct Day5 {}
+
+impl Solution for Day5 {
+    fn part_a(&self, input: String) -> String {
+        let data = input
+            .lines()
+            .map(|x| {
+                let coords_set = x
+                    .split(" -> ")
+                    .map(|y| {
+                        let coords = y
+                            .split(",")
+                            .map(|z| z.parse::<usize>().unwrap())
+                            .collect::<Vec<usize>>();
+
+                        (coords[0], coords[1])
+                    })
+                    .collect::<Vec<_>>();
+
+                (coords_set[0], coords_set[1])
+            })
+            .collect::<Vec<_>>();
+
+        let mut map = Map::new(1000);
+
+        for line in data {
+            let straight_line = map.straight_line(
+                line.0 .0 as isize,
+                line.0 .1 as isize,
+                line.1 .0 as isize,
+                line.1 .1 as isize,
+            );
+
+            match straight_line {
+                Some(x) => {
+                    for point in x {
+                        map.add_to_spot(point.0 as usize, point.1 as usize);
+                    }
+                }
+                None => (),
+            }
+        }
+
+        let mut sum = 0;
+        for row in map.data {
+            for num in row {
+                if num > 1 {
+                    sum += 1;
+                }
+            }
+        }
+
+        sum.to_string()
+    }
+    fn part_b(&self, input: String) -> String {
+        let data = input
+            .lines()
+            .map(|x| {
+                let coords_set = x
+                    .split(" -> ")
+                    .map(|y| {
+                        let coords = y
+                            .split(",")
+                            .map(|z| z.parse::<usize>().unwrap())
+                            .collect::<Vec<usize>>();
+
+                        (coords[0], coords[1])
+                    })
+                    .collect::<Vec<_>>();
+
+                (coords_set[0], coords_set[1])
+            })
+            .collect::<Vec<_>>();
+
+        let mut map = Map::new(1000);
+
+        for line in data {
+            map.line(
+                line.0 .0 as isize,
+                line.0 .1 as isize,
+                line.1 .0 as isize,
+                line.1 .1 as isize,
+            );
+        }
+
+        let mut sum = 0;
+        for row in map.data {
+            for num in row {
+                if num > 1 {
+                    sum += 1;
+                }
+            }
+        }
+
+        sum.to_string()
+    }
+}
+struct Map {
+    data: Vec<Vec<usize>>,
+    size: usize,
+}
+
+impl Map {
+    fn new(size: usize) -> Map {
+        Map {
+            data: vec![vec![0; size]; size],
+            size,
+        }
+    }
+    fn line(&mut self, x1: isize, y1: isize, x2: isize, y2: isize) {
+        if let Some(line) = self.straight_line(x1, y1, x2, y2) {
+            for point in line {
+                self.add_to_spot(point.0 as usize, point.1 as usize);
+            }
+        } else if let Some(line) = self.diagonal_line(x1, y1, x2, y2) {
+            for point in line {
+                self.add_to_spot(point.0 as usize, point.1 as usize);
+            }
+        }
+    }
+    fn straight_line(
+        &mut self,
+        x1: isize,
+        y1: isize,
+        x2: isize,
+        y2: isize,
+    ) -> Option<Vec<(isize, isize)>> {
+        let mut points = vec![];
+        if x1 == x2 {
+            let x = x1;
+            let range = if y1 <= y2 { y1..y2 + 1 } else { y2..y1 + 1 };
+
+            for y in range {
+                points.push((x, y));
+            }
+        } else if y1 == y2 {
+            let y = y1;
+            let range = if x1 <= x2 { x1..x2 + 1 } else { x2..x1 + 1 };
+
+            for x in range {
+                points.push((x, y));
+            }
+        }
+
+        if !points.is_empty() {
+            Some(points)
+        } else {
+            None
+        }
+    }
+    fn diagonal_line(
+        &mut self,
+        x1: isize,
+        y1: isize,
+        x2: isize,
+        y2: isize,
+    ) -> Option<Vec<(isize, isize)>> {
+        let mut points = vec![];
+        if (x1 - x2).abs() == (y1 - y2).abs() {
+            let mut x = x1;
+            let x_sign = -1 * (x1 - x2).signum();
+            let mut y = y1;
+            let y_sign = -1 * (y1 - y2).signum();
+
+            loop {
+                points.push((x, y));
+
+                if x == x2 && y == y2 {
+                    break;
+                } else {
+                    x += x_sign;
+                    y += y_sign;
+                }
+            }
+        }
+
+        if !points.is_empty() {
+            Some(points)
+        } else {
+            None
+        }
+    }
+    fn add_to_spot(&mut self, x: usize, y: usize) {
+        for input in [x, y] {
+            if input > self.size {
+                panic!("Coordinate out of bounds!");
+            }
+        }
+
+        self.data[y][x] += 1;
+    }
+    fn display_map(&self) {
+        let data = self.data.clone();
+
+        println!(
+            "{}",
+            data.iter()
+                .map(|row| { row.iter().map(|x| { x.to_string() }).collect::<String>() })
+                .collect::<Vec<String>>()
+                .join("\n")
+        );
+    }
+}
--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -2,12 +2,14 @@ mod day_1;
 mod day_2;
 mod day_3;
 mod day_4;
+mod day_5;
 
 use crate::common::Solution;
 
-pub const ALL: [&dyn Solution; 4] = [
+pub const ALL: [&dyn Solution; 5] = [
     &day_1::Day1 {},
     &day_2::Day2 {},
     &day_3::Day3 {},
     &day_4::Day4 {},
+    &day_5::Day5 {},
 ];
