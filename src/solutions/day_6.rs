690208cd7ee60d9efbd9707412609cc800fb8913
Eric Bishop <futureflyer.eric@gmail.com>
2022-06-01 16:21:44 -0600

    feat: Day 6 part 2
    Much faster, kinda silly that I didn't do this to begin with. Can't know what part 2 was before doing part 1 though, so keeping it adaptable and close to the problem that I know is probably a good idea.

--- src/solutions/day_6.rs
+++ src/solutions/day_6.rs
@@ -36,12 +36,21 @@ impl Solution for Day6 {
 }
 
 struct FishPopulation {
-    fishes: Vec<usize>,
+    fish_groups: Vec<usize>,
+    fish_buffer: Vec<usize>,
 }
 impl FishPopulation {
     fn new(initial_population: Vec<usize>) -> Self {
+        let mut fish_groups = vec![0; 7];
+        let fish_buffer = vec![0; 2];
+
+        for fish in initial_population {
+            fish_groups[fish] += 1;
+        }
+
         Self {
-            fishes: initial_population,
+            fish_groups,
+            fish_buffer,
         }
     }
     fn simulate_days(&mut self, days: usize) {
@@ -50,26 +59,15 @@ impl FishPopulation {
         }
     }
     fn simulate_day(&mut self) {
-        let mut reproduced = 0;
+        let new_fish = self.fish_groups.remove(0);
+        self.fish_groups.push(new_fish);
+        self.fish_buffer.push(new_fish);
 
-        self.fishes = self
-            .fishes
-            .iter()
-            .map(|fish| {
-                if *fish == 0 {
-                    reproduced += 1;
-                    6
-                } else {
-                    fish - 1
-                }
-            })
-            .collect();
-
-        for _ in 0..reproduced {
-            self.fishes.push(8);
-        }
+        let last_fish = self.fish_groups.len() - 1;
+        let added_fish = self.fish_buffer.remove(0);
+        self.fish_groups[last_fish] += added_fish;
     }
     fn total_fish(&self) -> usize {
-        self.fishes.len()
+        self.fish_groups.iter().sum::<usize>() + self.fish_buffer.iter().sum::<usize>()
     }
 }
