f58cdcfa1b301a382837eb9cc6a46bc9e7b825a9
Eric Bishop <bisho.eric@gmail.com>
2025-09-29 00:24:14 -0600

    Part 2

--- src/year_2024/day_01.rs
+++ src/year_2024/day_01.rs
@@ -1,30 +1,12 @@
+use std::collections::HashMap;
+
 use crate::shared::Solution;
 
 pub struct Day01 {}
 
 impl Solution for Day01 {
     fn part_a(&self, input: String) -> String {
-        let (mut left_list, mut right_list): (Vec<_>, Vec<_>) = input
-            .lines()
-            .map(|line| {
-                let mut numbers = line.split_whitespace();
-                let first = numbers.next();
-                let second = numbers.next();
-
-                match (first, second) {
-                    (Some(first), Some(second)) => (first, second),
-                    _ => panic!("Line did not contain two numbers"),
-                }
-            })
-            .map(|(lhs, rhs)| {
-                (
-                    lhs.parse::<i64>()
-                        .expect("Left side could not be parsed as a number"),
-                    rhs.parse::<i64>()
-                        .expect("Right side could not be parsed as a number"),
-                )
-            })
-            .unzip();
+        let (mut left_list, mut right_list) = parse(input);
 
         left_list.sort();
         right_list.sort();
@@ -38,6 +20,51 @@ impl Solution for Day01 {
     }
 
     fn part_b(&self, input: String) -> String {
-        todo!()
+        let (left_list, right_list) = parse(input);
+
+        let rhs_hashmap = right_list
+            .into_iter()
+            .fold(HashMap::new(), |mut hashmap, curr| {
+                hashmap
+                    .entry(curr)
+                    .and_modify(|entry| *entry += 1)
+                    .or_insert(1);
+
+                hashmap
+            });
+
+        left_list
+            .iter()
+            .map(|num| {
+                let rhs_count = rhs_hashmap.get(num).unwrap_or(&0);
+
+                num * rhs_count
+            })
+            .sum::<i64>()
+            .to_string()
     }
+}
+
+fn parse(input: String) -> (Vec<i64>, Vec<i64>) {
+    input
+        .lines()
+        .map(|line| {
+            let mut numbers = line.split_whitespace();
+            let first = numbers.next();
+            let second = numbers.next();
+
+            match (first, second) {
+                (Some(first), Some(second)) => (first, second),
+                _ => panic!("Line did not contain two numbers"),
+            }
+        })
+        .map(|(lhs, rhs)| {
+            (
+                lhs.parse::<i64>()
+                    .expect("Left side could not be parsed as a number"),
+                rhs.parse::<i64>()
+                    .expect("Right side could not be parsed as a number"),
+            )
+        })
+        .unzip()
 }
